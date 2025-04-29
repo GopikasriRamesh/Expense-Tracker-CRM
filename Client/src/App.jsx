470222d77c936f826ee53b7e6e63875bf1e2e768
GopikasriRamesh <srigopika270@gmail.com>
2025-04-29 11:59:29 +0530

    dashboard UI completed

---
+++ Client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import{BrowserRouter as Router,Routes,Route,Navigate,}from "react-router-dom";
+
+import Login from "./pages/auth/login";
+import Signup from "./pages/auth/signup";
+import Home from "./pages/dashboard/home";
+import Income from "./pages/dashboard/income";
+import Expense from "./pages/dashboard/expense";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>}/>
+          <Route path="/login" exact element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/dashboard" element={<Home/>}/>
+          <Route path="/income" element={<Income/>}/>
+          <Route path="/expense" element={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
+
+const Root=()=>{
+  const isAuthenticated=!!localStorage.getItem("token");
+  return isAuthenticated?(
+    <Navigate to="/dashboard"/>):(<Navigate to="/login"/>);
+};
+
---
+++ Client/src/pages/auth/login.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import AuthLayout from '../../components/layouts/authLayout';
+import { useNavigate, Link } from "react-router-dom";
+import Input from '../../components/layouts/inputs/Input';
+import { validateEmail } from '../../utils/helper';
+
+const Login = () => {
+  const [email, setemail] = useState("");
+  const [password, setpasword] = useState("");
+  const [error, seterror] = useState(null);
+
+  const navigate=useNavigate();
+//handel Login submit
+  const handelLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      seterror("enter valid email address!!");
+      return;
+    }
+    if(!password){
+      seterror("enter valid password")
+      return;
+    }
+
+    seterror("");
+
+    //Login API call
+    
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center px-6 pt-12">
+        <h3 className='text-xl font-semibold text-black mb-2'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Please enter your login details!!
+        </p>    
+        <form onSubmit={handelLogin}>
+          <Input 
+            value={email} 
+            onChange={({ target }) => setemail(target.value)} 
+            label="Email Address"
+            placeholder="abc@gmail.com" 
+            type="text" 
+          />
+          <Input 
+            value={password} 
+            onChange={({ target }) => setpasword(target.value)} 
+            label="Password"
+            placeholder="Min 8 characters" 
+            type="password" 
+          />
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+          <button type="submit" className='btn-primary'>LOGIN</button>
+          <p className='text-[13px] text-slate-800 mt-3'>
+            Don't have an account?!{" "}
+            <Link className='font-medium text-primary underline' to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </form>  
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
